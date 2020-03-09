4e2bf24a6a8830e0156b4b147def7dd0bea686f6
Sancia Fernandes <sanciafernandes2001@gmail.com>
2020-03-09 17:52:03 +0530

    updated values

--- app/src/main/java/com/example/bill_details_app/MainActivity.java
+++ app/src/main/java/com/example/bill_details_app/MainActivity.java
@@ -8,11 +8,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,8 +32,10 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     static String TAG = "test";
     private Button login_button;
+    private EditText uid;
+    private EditText pwd;
 
-    void test( String name, String add) {
+    void test( String name,  String add, String phone, String userid, String pasword) {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -29,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
         Map<String, Object> user = new HashMap<>();
         user.put("name", name);
         user.put("add", add);
-        //user.put("phone", phone);
+        user.put("phone",phone);
+        user.put("userid",userid);
+        user.put("password",pasword);
+        //+++++++user.put("phone", phone);
 
 // Add a new document with a generated ID
         db.collection("users")
@@ -46,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(TAG, "Error adding document", e);
                     }
                 });
+
+        // Create a reference to the cities collection
+        CollectionReference users = db.collection("users");
+        // Create a query against the collection.
+        Query query = users.whereEqualTo("userid", "jaison10");
+
+
+
+
     }
 
     @Override
@@ -55,29 +81,65 @@ public class MainActivity extends AppCompatActivity {
 
         // Access a Cloud Firestore instance from your Activity
 
-         test("Sancia", "Manglore");
+        test("Sancia", "Manglore", "9963029323", "sancia10", "sancia4ever");
 
-        Button login_button = (Button) findViewById(R.id.login_button);
+        test("Jaison", "Manglore", "9497886214", "jaison10", "jaison4ever");
+
+        login_button = (Button) findViewById(R.id.login_button);
         login_button.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, profile.class));
+                if (v.getId() == R.id.login_button) {
+                    openprofile();
+                }
             }
         });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient GoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+
+
+
     }
 
-
-    public void onClick(View view) {
-        if (view.getId() == R.id.login_button) {
-            openprofile();
-        }
-
+    private void updateUI(GoogleSignInAccount account) {
+        Log.d("The result is", String.valueOf(account));
     }
+
 
     public void openprofile() {
+        EditText uid=findViewById(R. id. username);
+        EditText pwd=findViewById(R. id. password);
+        String uidvalue=uid.getText().toString();
+        String pwdvalue =pwd.getText().toString();
+
         Intent intent = new Intent(this, profile.class);
+
+
+
+        try {
+            Bundle bundle = new Bundle();
+
+//Add your data to bundle
+            bundle.putString("USERNAME", uidvalue);
+            bundle.putString("PASSWORD", pwdvalue);
+//Add the bundle to the intent
+            intent.putExtras(bundle);
+        }
+        catch (Exception e){
+            Log.d("Exception:", "Ëxception need to be handled");
+        }
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/bill_details_app/profile.java
+++ app/src/main/java/com/example/bill_details_app/profile.java
@@ -9,17 +9,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.reflect.Member;
+import java.util.Map;
 
 public class profile extends AppCompatActivity {
     TextView Name, Address, phone;
 
     Button enter_previous;
-    DatabaseReference reff;
-    Member classprofile;
+    Bundle bundle ;//= getIntent().getExtras();
+
+    //Extract the data…
+    String username ;//= bundle.getString("USERNAME");
+    String password ;//= bundle.getString("PASSWORD");
+
+    private static final String TAG = "profile";
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    //DocumentReference docRef = db.collection("users").document("sancia10");
 
 
     @Override
@@ -27,6 +52,28 @@ public class profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        bundle = getIntent().getExtras();
+         username = bundle.getString("USERNAME");
+         password = bundle.getString("PASSWORD");
+
+         Log.e(TAG, "username is " + username);
+
+        db.collection("users")
+                .whereEqualTo("userid", "jaison10").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task){
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.e(TAG, document.getId() + " => " + document.getData());
+                        Map<String, Object> data = document.getData();
+                        Log.e(TAG, "" + data.get("add") + " " + data.get("phone"));
+                        Log.e(TAG, "In profile page");
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
 
         enter_previous = (Button) findViewById(R.id.enter_previous);
         Address = (TextView) findViewById(R.id.Address) ;
@@ -34,48 +81,16 @@ public class profile extends AppCompatActivity {
         Name=(TextView) findViewById(R.id.Name);
 
         enter_previous.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 opendetails_user();
-
-            }
-        });
-        enter_previous.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Long phone=Long.parseLong(enter_previous.getText().toString().trim());
-//                classprofile.getName(Name.getText().toString().trim());
-//                classprofile.getName(Name);
-//                reff.child(String.valueOf(DatabaseReference))
             }
         });
 
-       reff = FirebaseDatabase.getInstance().getReference().child("Member");
-        classprofile=new Member() {
-            @NonNull
-            @Override
-            public Class<?> getDeclaringClass() {
-                return null;
-            }
 
-            @NonNull
-            @Override
-            public String getName() {
-                return null;
-            }
+        }
 
-            @Override
-            public int getModifiers() {
-                return 0;
-            }
-
-            @Override
-            public boolean isSynthetic() {
-                return false;
-            }
-        };
-    }
 
     public  void opendetails_user() {
         Intent intent = new Intent(this, details_user.class);
